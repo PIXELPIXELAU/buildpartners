18d64c51198b1411302b14782c2edd65b158cabe
Gary Swanepoel <gary@pixelpixel.com.au>
2021-08-04 15:28:52 +1000

    merge with head

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,37 +11,63 @@ window.app = {
   if (burger) {
     burger.addEventListener('click', () => {
       const menu = document.getElementById(burger.dataset.target);
-
       burger.classList.toggle('is-active');
       menu.classList.toggle('is-active');
     })
   }
-})()
+
+const form = document.querySelector("form");
+if (form.length > 0) {
+  form.addEventListener("submit", handleSubmit);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('pizzaOrder');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
+  }
+}
+
+  })();
 
 $(function(){
-  const scrollLeft = $('.scroll__wrapper .columns').scrollLeft();
-  const count = $('.scroll__wrapper .column').length
-  const scrollBy = 240;
-  // $('.scroll__wrapper .columns').scrollLeft(0);
-  $('.scroll__wrapper .columns')[0].scroll({
-    left: 0,
-    top: 0,
-    behavior: 'smooth'
-});
-  for (var i=0;i<count;i++) {
-    console.log(i)
-    setTimeout(function(index){
-      console.log(index,scrollLeft,count,scrollBy,(scrollBy * index))
-      $('.scroll__wrapper .columns')[0].scroll({
-          left: scrollBy * index ,
-          top: 0,
-          behavior: 'smooth'
-      });
-    },3000*i,(i))
-    
-  }
+
+  //Form Submissions
+
+
   
-})
+  //Scrollable logos
+  if ($('.scroll__wrapper').lenght > 0 ) {
+    const scrollLeft = $('.scroll__wrapper .columns').scrollLeft();
+    const count = $('.scroll__wrapper .column').length
+    const scrollBy = 240;
+    // $('.scroll__wrapper .columns').scrollLeft(0);
+    $('.scroll__wrapper .columns')[0].scroll({
+      left: 0,
+      top: 0,
+      behavior: 'smooth'
+  });
+    for (var i=0;i<count;i++) {
+      console.log(i)
+      setTimeout(function(index){
+        console.log(index,scrollLeft,count,scrollBy,(scrollBy * index))
+        $('.scroll__wrapper .columns')[0].scroll({
+            left: scrollBy * index ,
+            top: 0,
+            behavior: 'smooth'
+        });
+      },3000*i,(i))
+      
+    }
+  };
+    
+
+  
+});
 // $(function() {
 //   $('.navbar-burger').on("click", function() {
 //     $(".navbar-burger").toggleClass("is-active");
